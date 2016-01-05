696b48afb2645a3d367986dc20f06cd753e19b64
ACLTC <acltc@ACLTCs-MacBook-Air.local>
2016-01-04 20:22:28 -0800

    finished changing to angular objects

--- app/assets/javascripts/example_ctrl.js
+++ app/assets/javascripts/example_ctrl.js
@@ -3,10 +3,44 @@
 
   angular.module("app").controller("exampleCtrl", function($scope) {
 
-    $scope.reviews = ["Angular is awesome", "Angular is the bomb", "Angular is bougie"];
+    $scope.reviews = [
+    {
+      text: "Angular is awesome",
+      rating: 5,
+      reviewer: "Burton Wax"
+    },
+    {
+      text: "Angular is the bomb",
+      rating: 4,
+      reviewer: "Burton Wax"
+    },
+    {
+      text: "Angular is tragic",
+      rating: 1,
+      reviewer: "Burton Wax"
+    }
+    ];
 
     $scope.addReview = function(review) {
-      $scope.reviews.push(review);
+      if (review) {
+        // var review = {
+        //   text: reviewText,
+        //   rating: reviewRating,
+        //   reviewer: reviewReviewer
+        // };
+        $scope.reviews.push(review);
+        // $scope.reviewText = null;
+        // $scope.reviewRating = null;
+        // $scope.reviewReviewer = null;
+      }
+    }
+
+    $scope.ifPositive = function(review) {
+      return review.text.indexOf("stupid") === -1;
+    }
+
+    $scope.completeReview = function(index) {
+      $scope.reviews.splice(index, 1);
     }
 
 
